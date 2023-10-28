67badd3cb130ada888fc6bef5d075db5511d3003
Leno <24483665+jotaleno@users.noreply.github.com>
2023-10-28 20:47:21 +0100

    Add telescope

--- .config/nvim/lua/leno/core/keymaps.lua
+++ .config/nvim/lua/leno/core/keymaps.lua
@@ -2,7 +2,14 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
-keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) 
-keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" })
-keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) 
-keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
+-- NvimTree
+keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<cr>", { desc = "Toggle file explorer" }) 
+keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<cr>", { desc = "Toggle file explorer on current file" })
+keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<cr>", { desc = "Collapse file explorer" }) 
+keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<cr>", { desc = "Refresh file explorer" })
+
+-- Telescope
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Telecospe file files" })
+keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Telescope live grep" })
+keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Telescope buffers" })
+keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { desc = "Telescope help tags" })
--- .config/nvim/lua/leno/plugins/nvim-tree.lua
+++ .config/nvim/lua/leno/plugins/nvim-tree.lua
@@ -1,37 +1,39 @@
--- return {
---  "nvim-tree/nvim-tree.lua",
---  dependencies = {
---    "nvim-tree/nvim-web-devicons"
---  },
---  config = function()
---    -- disable netrw at the very start of your init.lua
---    vim.g.loaded_netrw = 1
---    vim.g.loaded_netrwPlugin = 1
---
---    -- set termguicolors to enable highlight groups
---    vim.opt.termguicolors = true
---
---    -- empty setup using defaults
---    require("nvim-tree").setup()
---  end
--- }
-
 return {
-  "preservim/nerdtree",
+   "nvim-tree/nvim-tree.lua",
   dependencies = {
-    "nvim-tree/nvim-web-devicons",
-    "tiagofumo/vim-nerdtree-syntax-highlight",
-    "ryanoasis/vim-devicons"
+    "nvim-tree/nvim-web-devicons"
   },
   config = function()
-    local global = vim.g
+    -- disable netrw at the very start of your init.lua
+   vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
 
-    global.NERDTreeShowHidden = 1
+    -- set termguicolors to enable highlight groups
+    vim.opt.termguicolors = true
 
-    global.NERDTreeDirArrowExpandable = ''
-    global.NERDTreeDirArrowCollapsible = ''
-
-    -- Start NERDTree and leave the cursor in it.
-    vim.cmd [[ autocmd VimEnter * NERDTree ]]
-  end 
+    -- empty setup using defaults
+    require("nvim-tree").setup()
+    
+    vim.cmd [[ autocmd VimEnter * NvimTreeFocus ]]
+  end
 }
+
+-- return {
+--  "preservim/nerdtree",
+--  dependencies = {
+--    "nvim-tree/nvim-web-devicons",
+--    "tiagofumo/vim-nerdtree-syntax-highlight",
+--    "ryanoasis/vim-devicons"
+--  },
+--  config = function()
+--    local global = vim.g
+--
+--    global.NERDTreeShowHidden = 1
+
+--    global.NERDTreeDirArrowExpandable = ''
+--    global.NERDTreeDirArrowCollapsible = ''
+--    global.webdevicons_conceal_nerdtree_brackets=0
+--    -- Start NERDTree and leave the cursor in it.
+--    vim.cmd [[ autocmd VimEnter * NERDTree ]]
+--  end 
+-- }
