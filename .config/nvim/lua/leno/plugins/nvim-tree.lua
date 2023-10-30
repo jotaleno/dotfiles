f9ab8595b69e5db23126e8a636b05318bc835cc1
Leno <24483665+jotaleno@users.noreply.github.com>
2023-10-30 20:07:57 +0000

    Add nvim formater and lsp

--- .config/nvim/lua/leno/plugins/nvim-tree.lua
+++ .config/nvim/lua/leno/plugins/nvim-tree.lua
@@ -1,21 +1,68 @@
 return {
   "nvim-tree/nvim-tree.lua",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons"
-  },
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    -- disable netrw at the very start of your init.lua
+    local nvimtree = require("nvim-tree")
+
+    -- recommended settings from nvim-tree documentation
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
 
     -- set termguicolors to enable highlight groups
-    vim.opt.termguicolors = true
+    -- vim.opt.termguicolors = true
 
-    -- empty setup using defaults
-    require("nvim-tree").setup()
+    -- change color for arrows in tree to light blue
+    vim.cmd([[ highlight NvimTreeFolderArrowClosed guifg=#3FC5FF ]])
+    vim.cmd([[ highlight NvimTreeFolderArrowOpen guifg=#3FC5FF ]])
 
-    vim.cmd [[ autocmd VimEnter * :NvimTreeToggle ]]
-  end
+    -- configure nvim-tree
+    nvimtree.setup({
+      view = {
+        width = 35,
+        relativenumber = true,
+      },
+      -- change folder arrow icons
+      renderer = {
+        indent_markers = {
+          enable = true,
+        },
+        icons = {
+          glyphs = {
+            folder = {
+              arrow_closed = "", -- arrow when folder is closed
+              arrow_open = "", -- arrow when folder is open
+            },
+          },
+        },
+      },
+      -- disable window_picker for
+      -- explorer to work well with
+      -- window splits
+      actions = {
+        open_file = {
+          window_picker = {
+            enable = false,
+          },
+        },
+      },
+      filters = {
+        custom = { ".DS_Store" },
+      },
+      git = {
+        ignore = false,
+      },
+    })
+
+    vim.cmd([[ autocmd VimEnter * :NvimTreeToggle ]])
+
+    -- set keymaps
+    local keymap = vim.keymap -- for conciseness
+
+    keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
+    keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
+    keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
+    keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
+  end,
 }
 
 -- return {
@@ -35,5 +82,5 @@ return {
 --    global.webdevicons_conceal_nerdtree_brackets=0
 --    -- Start NERDTree and leave the cursor in it.
 --    vim.cmd [[ autocmd VimEnter * NERDTree ]]
---  end 
+--  end
 -- }
